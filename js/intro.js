b01e7f793093a093fd32f90c6bc321b47c60f7c6
Divya <drmohan@andrew.cmu.edu>
2017-05-17 23:29:40 -0700

    Readme collapsing transition

--- js/intro.js
+++ js/intro.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
 });
 
 function openFolder(folder){
-    console.log(folder);
     var path = "partials/" + folder + ".html"
     $('#table-home').load(path);
 }
@@ -22,26 +21,48 @@ function openFolder(folder){
 function openReadMe(){
     $('.intro-text').transition('fade');
     
-    $('#table-home').css("top", "15%")
-    $('#table-home').css("display", "none")
+    $("#table-home").css("cssText", "display: none !important;");
+    $('#table-home').css("top", "15%");
     
     $('#table-home').transition({
         animation : 'fade up',
         duration  : 1000,
-        onComplete : function() {
-//          $('#table-home').css("margin-top", "15%")
-            
-            $('#table-home').css("position", "relative")
+        onComplete : function() {            
+            $('#table-home').css("position", "relative");
             
             var rm = document.createElement("div");
             rm.id = "readMe";
-//            rm.className = "ui top attached segment";
             $('.github').append(rm);
             $("#readMe").load("partials/readme.html");
+            
+            $("#folder-path").css("display", "inline");
         }
     });
     
     $('#table-body').css("display", "none")
+    
+}
+
+function collapseToHome(folder){
+    
+    $('.intro-text').transition('fade');
+    $('#readMe').css("display", "none");
+    $("#folder-path").css("display", "none");
+    
+    var path = "partials/" + folder + ".html"
+    $('#table-home').load(path);
+    
+    $("#table-home").css("cssText", "display: none !important;");
+    $('#table-home').transition({
+        animation : 'fade down',
+        duration  : 1000,
+        onComplete : function() {   
+            $('#table-home').css("position", "fixed");
+            $('#table-home').css("top", "40%");
+            $("#table-home").css("display", "inline");
+        }
+    });
+    
     
     
 }
