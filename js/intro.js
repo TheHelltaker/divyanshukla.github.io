641c0ba8501fe3fabd6ee47cb8e7095c690339e9
Divya <drmohan@andrew.cmu.edu>
2017-05-17 22:51:54 -0700

    readme transitions

--- js/intro.js
+++ js/intro.js
@@ -18,3 +18,30 @@ function openFolder(folder){
     var path = "partials/" + folder + ".html"
     $('#table-home').load(path);
 }
+
+function openReadMe(){
+    $('.intro-text').transition('fade');
+    
+    $('#table-home').css("top", "15%")
+    $('#table-home').css("display", "none")
+    
+    $('#table-home').transition({
+        animation : 'fade up',
+        duration  : 1000,
+        onComplete : function() {
+//          $('#table-home').css("margin-top", "15%")
+            
+            $('#table-home').css("position", "relative")
+            
+            var rm = document.createElement("div");
+            rm.id = "readMe";
+//            rm.className = "ui top attached segment";
+            $('.github').append(rm);
+            $("#readMe").load("partials/readme.html");
+        }
+    });
+    
+    $('#table-body').css("display", "none")
+    
+    
+}
